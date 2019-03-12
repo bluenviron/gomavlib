15fdb22359b1f8dff9b01d4a70b8e87dee9f8982
gswly <46489434+gswly@users.noreply.github.com>
2019-03-13 00:52:20 +0100

    deterministic dialgen

--- dialects/matrixpilot/dialect.go
+++ dialects/matrixpilot/dialect.go
@@ -2086,7 +2086,7 @@ type MessagePlayTune struct {
 	TargetSystem    uint8
 	TargetComponent uint8
 	Tune            string `mavlen:"30"`
-	Tune2           string `mavlen:"200",mavext:"true"`
+	Tune2           string `mavext:"true",mavlen:"200"`
 }
 
 func (*MessagePlayTune) GetId() uint32 {

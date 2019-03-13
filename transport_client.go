45b0693b55fddbfa1dca0fc16945e9608c95bde5
gswly <46489434+gswly@users.noreply.github.com>
2019-03-13 15:11:17 +0100

    transport client now depends on transport custom

--- transport_client.go
+++ transport_client.go
@@ -2,7 +2,9 @@ package gomavlib
 
 import (
 	"fmt"
+	"io"
 	"net"
+	"sync"
 	"time"
 )
 
@@ -48,9 +50,11 @@ func (conf TransportUdpClient) init(n *Node) (transport, error) {
 }
 
 type transportClient struct {
-	conf      transportClientConf
-	node      *Node
-	terminate chan struct{}
+	conf       transportClientConf
+	mutex      sync.Mutex
+	terminated bool
+	terminate  chan struct{}
+	conn       io.ReadWriteCloser
 }
 
 func initTransportClient(node *Node, conf transportClientConf) (transport, error) {
@@ -60,90 +64,112 @@ func initTransportClient(node *Node, conf transportClientConf) (transport, error
 	}
 
 	t := &transportClient{
-		conf: conf,
-		node: node,
-	}
-	return t, nil
-}
-
-func (t *transportClient) closePrematurely() {
-}
-
-func (t *transportClient) do() {
-	// reuse the same tconn such that it can be used as map key
-	tconn := &TransportChannel{
-		transport: t,
-		writer:    nil,
+		conf:      conf,
+		terminate: make(chan struct{}, 1),
 	}
 
+	tc := &TransportCustom{
+		ReadWriteCloser: t,
+	}
+	return tc.init(node)
+}
+
+func (t *transportClient) Close() error {
+	t.mutex.Lock()
+	defer t.mutex.Unlock()
+
+	if t.terminated == true {
+		return nil
+	}
+	t.terminate <- struct{}{}
+	return nil
+}
+
+func (t *transportClient) Write(buf []byte) (int, error) {
+	t.mutex.Lock()
+	defer t.mutex.Unlock()
+
+	if t.conn != nil {
+		return t.conn.Write(buf)
+	}
+	return 0, fmt.Errorf("disconnected")
+}
+
+func (t *transportClient) Read(buf []byte) (int, error) {
 	for {
-		// solve address and connect
-		// in UDP, the only possible error is a DNS failure
-		// in TCP, the handshake must be completed
-		var rawConn net.Conn
-		dialDone := make(chan struct{}, 1)
-		go func() {
-			var network string
-			if t.conf.isUdp() == true {
-				network = "udp4"
-			} else {
-				network = "tcp4"
-			}
-			var err error
-			rawConn, err = net.DialTimeout(network, t.conf.getAddress(), netConnectTimeout)
-			if err != nil {
-				rawConn = nil // ensure rawConn is nil in case of error
-			}
-			dialDone <- struct{}{}
-		}()
+		// mutex is not necessary since Read() is the only one that can fill and empty conn
+		if t.conn == nil {
+			// solve address and connect
+			// in UDP, the only possible error is a DNS failure
+			// in TCP, the handshake must be completed
+			var rawConn net.Conn
+			dialDone := make(chan struct{}, 1)
+			go func() {
+				var network string
+				if t.conf.isUdp() == true {
+					network = "udp4"
+				} else {
+					network = "tcp4"
+				}
+				var err error
+				rawConn, err = net.DialTimeout(network, t.conf.getAddress(), netConnectTimeout)
+				if err != nil {
+					rawConn = nil // ensure rawConn is nil in case of error
+				}
+				dialDone <- struct{}{}
+			}()
 
-		select {
-		case <-dialDone:
-		case <-t.node.terminate:
-			return
-		}
-
-		// wait some seconds before reconnecting
-		if rawConn == nil {
-			timer := time.NewTimer(netReconnectPeriod)
 			select {
-			case <-timer.C:
-				continue
-			case <-t.node.terminate:
-				return
+			case <-dialDone:
+			case <-t.terminate:
+				return 0, fmt.Errorf("terminated")
 			}
+
+			// wait some seconds before reconnecting
+			if rawConn == nil {
+				timer := time.NewTimer(netReconnectPeriod)
+				select {
+				case <-timer.C:
+					continue
+				case <-t.terminate:
+					return 0, fmt.Errorf("terminated")
+				}
+			}
+
+			func() {
+				t.mutex.Lock()
+				defer t.mutex.Unlock()
+				t.conn = &netTimedConn{rawConn}
+			}()
 		}
 
-		conn := &netTimedConn{rawConn}
-		tconn.writer = conn
-		t.node.addTransportChannel(tconn)
-
+		var n int
+		var err error
 		readDone := make(chan struct{})
 		go func() {
 			defer func() { readDone <- struct{}{} }()
-			defer t.node.removeTransportChannel(tconn)
-
-			var buf [netBufferSize]byte
-			for {
-				n, err := conn.Read(buf[:])
-				if err != nil {
-					break
-				}
-				t.node.processBuffer(tconn, buf[:n])
-			}
+			n, err = t.conn.Read(buf)
 		}()
 
 		select {
-		// unexpected error, restart connection
 		case <-readDone:
-			conn.Close()
-			continue
-
-		// terminated
-		case <-t.node.terminate:
-			conn.Close()
+		case <-t.terminate:
+			t.conn.Close()
 			<-readDone
-			return
+			return 0, fmt.Errorf("terminated")
 		}
+
+		// unexpected error, restart connection
+		if err != nil {
+			t.conn.Close()
+			func() {
+				t.mutex.Lock()
+				defer t.mutex.Unlock()
+				t.conn = nil
+			}()
+			continue
+		}
+
+		return n, nil
 	}
 }

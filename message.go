65ec7587ed949c2a66728554ead89fe53d5b3dfc
gswly <46489434+gswly@users.noreply.github.com>
2019-03-14 13:12:49 +0100

    documentation

--- message.go
+++ message.go
@@ -1,14 +1,21 @@
 package gomavlib
 
+// Message is the interface that all mavlink messages must implements.
+// Furthermore, messages are required to be labeled MessageNameOfMessage.
 type Message interface {
 	GetId() uint32
 }
 
+// MessageRaw is a special struct that contains a byte-encoded message,
+// available in Content. Is is used:
+// - as intermediate step in the encoding/decoding process
+// - when the parser receives an unknown message
 type MessageRaw struct {
 	Id      uint32
 	Content []byte
 }
 
+// GetId() implements the message interface.
 func (m *MessageRaw) GetId() uint32 {
 	return m.Id
 }
--- x25.go
+++ x25.go
@@ -5,7 +5,7 @@ import (
 )
 
 // Hash16 is an interface modeled on the standard hash.Hash32 and hash.Hash64.
-// it is implemented by 16-bits hash functions. In this library, it is used byX25.
+// In this library, it is implemented by NewX25().
 type Hash16 interface {
 	hash.Hash
 	Sum16() uint16

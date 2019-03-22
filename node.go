cd7d746ce0a1d60cbec3a796543bd326aa9544a2
gswly <46489434+gswly@users.noreply.github.com>
2019-03-22 19:50:28 +0100

    update docs

--- node.go
+++ node.go
@@ -325,11 +325,14 @@ func (res *NodeEventFrame) ComponentId() byte {
 	return res.Frame.GetComponentId()
 }
 
-// Events returns a channel from which receiving events. Possible events are:
-// - *NodeEventFrame
-// - *NodeEventParseError
-// - *NodeEventChannelOpen
-// - *NodeEventChannelClose
+// Events returns a channel from which receiving events.
+//
+// Possible events are:
+//   *NodeEventFrame
+//   *NodeEventParseError
+//   *NodeEventChannelOpen
+//   *NodeEventChannelClose
+//
 // see individual events for meaning and content.
 func (n *Node) Events() chan NodeEvent {
 	return n.eventChan

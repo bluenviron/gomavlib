19d82269183ce07950dde18cd15f0f7e316553c3
gswly <46489434+gswly@users.noreply.github.com>
2019-03-22 21:39:32 +0100

    reorder structs

--- endpoint.go
+++ endpoint.go
@@ -4,19 +4,7 @@ import (
 	"io"
 )
 
-// EndpointChannel is a channel provided by a endpoint.
-type EndpointChannel struct {
-	desc      string
-	rwc       io.ReadWriteCloser
-	writeChan chan interface{}
-}
-
-// String implements fmt.Stringer and returns infos about channel.
-func (e *EndpointChannel) String() string {
-	return e.desc
-}
-
-// EndpointConf is the interface implemented by all endpoints.
+// EndpointConf is the interface implemented by all endpoint configurations.
 type EndpointConf interface {
 	init() (endpoint, error)
 }
@@ -29,14 +17,24 @@ type endpoint interface{}
 // endpoint that provides a single channel.
 // Read() must not return any error unless Close() is called.
 type endpointChannelSingle interface {
-	endpoint
 	Desc() string
 	io.ReadWriteCloser
 }
 
 // endpoint that provides multiple channels.
 type endpointChannelAccepter interface {
-	endpoint
 	Close() error
 	Accept() (endpointChannelSingle, error)
+}
+
+// EndpointChannel is a channel provided by a endpoint.
+type EndpointChannel struct {
+	desc      string
+	rwc       io.ReadWriteCloser
+	writeChan chan interface{}
+}
+
+// String implements fmt.Stringer and returns infos about channel.
+func (e *EndpointChannel) String() string {
+	return e.desc
 }

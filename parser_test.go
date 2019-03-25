6ecd824c8b19c4bd7fd0ea85489147ee6e0a5ab7
gswly <46489434+gswly@users.noreply.github.com>
2019-03-25 12:42:15 +0100

    add additional test

--- parser_test.go
+++ parser_test.go
@@ -7,38 +7,6 @@ import (
 	"testing"
 )
 
-func testFrameDecode(t *testing.T, dialect *Dialect, key *FrameSignatureKey, byts [][]byte, frames []Frame) {
-	for i, byt := range byts {
-		parser, err := NewParser(ParserConf{
-			Reader:         bytes.NewReader(byt),
-			Writer:         bytes.NewBuffer(nil),
-			Dialect:        dialect,
-			SystemId:       1,
-			SignatureInKey: key,
-		})
-		require.NoError(t, err)
-		frame, err := parser.Read()
-		require.NoError(t, err)
-		require.Equal(t, frames[i], frame)
-	}
-}
-
-func testFrameEncode(t *testing.T, dialect *Dialect, key *FrameSignatureKey, byts [][]byte, frames []Frame) {
-	for i, frame := range frames {
-		buf := bytes.NewBuffer(nil)
-		parser, err := NewParser(ParserConf{
-			Reader:   bytes.NewBuffer(nil),
-			Writer:   buf,
-			SystemId: 1,
-			Dialect:  dialect,
-		})
-		require.NoError(t, err)
-		err = parser.Write(frame, true)
-		require.NoError(t, err)
-		require.Equal(t, byts[i], buf.Bytes())
-	}
-}
-
 type MessageTest5 struct {
 	TestByte byte
 	TestUint uint32
@@ -72,8 +40,41 @@ var testDialect = MustDialect([]Message{
 	&MessageTest8{},
 })
 
+func testFrameDecode(t *testing.T, dialect *Dialect, key *FrameSignatureKey, byts [][]byte, frames []Frame) {
+	for i, byt := range byts {
+		parser, err := NewParser(ParserConf{
+			Reader:         bytes.NewReader(byt),
+			Writer:         bytes.NewBuffer(nil),
+			Dialect:        dialect,
+			SystemId:       1,
+			SignatureInKey: key,
+		})
+		require.NoError(t, err)
+		frame, err := parser.Read()
+		require.NoError(t, err)
+		require.Equal(t, frames[i], frame)
+	}
+}
+
+func testFrameEncode(t *testing.T, dialect *Dialect, key *FrameSignatureKey, byts [][]byte, frames []Frame) {
+	for i, frame := range frames {
+		buf := bytes.NewBuffer(nil)
+		parser, err := NewParser(ParserConf{
+			Reader:   bytes.NewBuffer(nil),
+			Writer:   buf,
+			SystemId: 1,
+			Dialect:  dialect,
+		})
+		require.NoError(t, err)
+		err = parser.Write(frame, true)
+		require.NoError(t, err)
+		require.Equal(t, byts[i], buf.Bytes())
+	}
+}
+
 var testFpV1Bytes = [][]byte{
 	[]byte("\xFE\x05\x27\x01\x02\x05\x10\x10\x10\x10\x10\xe5\x66"),
+	[]byte("\xFE\x05\x27\x01\x02\x08\x10\x10\x10\x10\x10\xfa\xc7"),
 }
 
 var testFpV1Frames = []Frame{
@@ -87,6 +88,16 @@ var testFpV1Frames = []Frame{
 		},
 		Checksum: 0x66e5,
 	},
+	&FrameV1{
+		SequenceId:  0x27,
+		SystemId:    0x01,
+		ComponentId: 0x02,
+		Message: &MessageRaw{
+			0x08,
+			[]byte("\x10\x10\x10\x10\x10"),
+		},
+		Checksum: 0xc7fa,
+	},
 }
 
 var testFpV1FramesDialect = []Frame{
@@ -99,6 +110,16 @@ var testFpV1FramesDialect = []Frame{
 			binary.LittleEndian.Uint32([]byte("\x10\x10\x10\x10")),
 		},
 		Checksum: 0x66e5,
+	},
+	&FrameV1{
+		SequenceId:  0x27,
+		SystemId:    0x01,
+		ComponentId: 0x02,
+		Message: &MessageTest8{
+			'\x10',
+			binary.LittleEndian.Uint32([]byte("\x10\x10\x10\x10")),
+		},
+		Checksum: 0xc7fa,
 	},
 }
 
@@ -193,12 +214,45 @@ mav.signing.timestamp = 2; \
 mav.signing.sign_outgoing = True; \
 mav.heartbeat_send(type=1, autopilot=2, base_mode=3, custom_mode=4, system_status=5); \
 f.seek(0); \
-print(f.read());"
+print(''.join('\\\x{:02x}'.format(x) for x in f.read()));"
+
+( docker build - -t temp << EOF
+FROM amd64/python:3-stretch
+RUN apt update && apt install -y --no-install-recommends \
+    git \
+    gcc \
+    python3-dev \
+    python3-setuptools \
+    python3-wheel \
+    python3-pip \
+    python3-future \
+    python3-lxml \
+    && pip3 install pymavlink
+EOF
+) && docker run --rm -it temp python3 -c \
+"import io; from pymavlink.dialects.v20 import ardupilotmega; \
+f = io.BytesIO(); \
+mav = ardupilotmega.MAVLink(f); \
+mav.signing.secret_key = (chr(79)*32).encode(); \
+mav.signing.link_id = 3; \
+mav.signing.timestamp = 4; \
+mav.signing.sign_outgoing = True; \
+mav.optical_flow_send(time_usec=1, sensor_id=2, flow_x=3, flow_y=4, flow_comp_m_x=5, flow_comp_m_y=6, quality=7, ground_distance=8, flow_rate_y=1); \
+f.seek(0); \
+print(''.join('\\\x{:02x}'.format(x) for x in f.read()));"
 
 */
 
+var testFpV2SigKey = NewFrameSignatureKey(bytes.Repeat([]byte("\x4F"), 32))
+
+var testFpV2SigDialect = MustDialect([]Message{
+	&MessageHeartbeat{},
+	&MessageOpticalFlow{},
+})
+
 var testFpV2SigBytes = [][]byte{
-	[]byte("\xfd\t\x01\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00\x01\x02\x03\x05\x03\xd9\xd1\x01\x02\x00\x00\x00\x00\x00\x0eG\x04\x0c\xef\x9b"),
+	[]byte("\xfd\x09\x01\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00\x01\x02\x03\x05\x03\xd9\xd1\x01\x02\x00\x00\x00\x00\x00\x0e\x47\x04\x0c\xef\x9b"),
+	[]byte("\xfd\x22\x01\x00\x00\x00\x00\x64\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\xa0\x40\x00\x00\xc0\x40\x00\x00\x00\x41\x03\x00\x04\x00\x02\x07\x00\x00\x00\x00\x00\x00\x80\x3f\x77\xfb\x03\x04\x00\x00\x00\x00\x00\xa8\x88\x09\x39\xb2\x60"),
 }
 
 var testFpV2SigFrames = []Frame{
@@ -216,43 +270,39 @@ var testFpV2SigFrames = []Frame{
 			SystemStatus:   5,
 			MavlinkVersion: 3,
 		},
-		Checksum:           53721,
+		Checksum:           0xd1d9,
 		SignatureLinkId:    1,
 		SignatureTimestamp: 2,
-		Signature:          &FrameSignature{14, 71, 4, 12, 239, 155},
+		Signature:          &FrameSignature{0x0e, 0x47, 0x04, 0x0c, 0xef, 0x9b},
 	},
-}
-
-var testFpV2Key = NewFrameSignatureKey(bytes.Repeat([]byte("\x4F"), 32))
-
-func TestParserV2Signature(t *testing.T) {
-	parser, err := NewParser(ParserConf{
-		Reader:   bytes.NewBuffer(nil),
-		Writer:   bytes.NewBuffer(nil),
-		SystemId: 1,
-	})
-	require.NoError(t, err)
-	sig := parser.Signature(&FrameV2{
+	&FrameV2{
 		IncompatibilityFlag: 0x01,
 		CompatibilityFlag:   0x00,
 		SequenceId:          0x00,
 		SystemId:            0x00,
 		ComponentId:         0x00,
-		Message: &MessageRaw{
-			Id:      0,
-			Content: []byte("\x04\x00\x00\x00\x01\x02\x03\x05\x03"),
+		Message: &MessageOpticalFlow{
+			TimeUsec:       1,
+			SensorId:       2,
+			FlowX:          3,
+			FlowY:          4,
+			FlowCompMX:     5,
+			FlowCompMY:     6,
+			Quality:        7,
+			GroundDistance: 8,
+			FlowRateY:      1,
 		},
-		Checksum:           53721,
-		SignatureLinkId:    1,
-		SignatureTimestamp: 2,
-	}, testFpV2Key)
-	require.Equal(t, &FrameSignature{14, 71, 4, 12, 239, 155}, sig)
+		Checksum:           0xfb77,
+		SignatureLinkId:    3,
+		SignatureTimestamp: 4,
+		Signature:          &FrameSignature{0xa8, 0x88, 0x9, 0x39, 0xb2, 0x60},
+	},
 }
 
 func TestParserV2SignatureDec(t *testing.T) {
-	testFrameDecode(t, MustDialect([]Message{&MessageHeartbeat{}}), testFpV2Key, testFpV2SigBytes, testFpV2SigFrames)
+	testFrameDecode(t, testFpV2SigDialect, testFpV2SigKey, testFpV2SigBytes, testFpV2SigFrames)
 }
 
 func TestParserV2SignatureEnc(t *testing.T) {
-	testFrameEncode(t, MustDialect([]Message{&MessageHeartbeat{}}), testFpV2Key, testFpV2SigBytes, testFpV2SigFrames)
+	testFrameEncode(t, testFpV2SigDialect, testFpV2SigKey, testFpV2SigBytes, testFpV2SigFrames)
 }

004ed7e1e967a782a1eb2e7b22ae429f26d5beab
aler9 <46489434+aler9@users.noreply.github.com>
2020-06-29 05:05:06 +0200

    (automatic) update dialects to rev https://github.com/mavlink/mavlink/tree/527abe53c4c9bf6fe811d5f8b236d6ecc73ded67

--- dialects/common/dialect.go
+++ dialects/common/dialect.go
@@ -2470,161 +2470,6 @@ func (e FIRMWARE_VERSION_TYPE) String() string {
 	return strconv.FormatInt(int64(e), 10)
 }
 
-// Flags to report status/failure cases for a power generator (used in GENERATOR_STATUS). Note that FAULTS are conditions that cause the generator to fail. Warnings are conditions that require attention before the next use (they indicate the system is not operating properly).
-type GENERATOR_STATUS_FLAG int
-
-const (
-	// Generator is off.
-	GENERATOR_STATUS_FLAG_OFF GENERATOR_STATUS_FLAG = 1
-	// Generator is ready to start generating power.
-	GENERATOR_STATUS_FLAG_READY GENERATOR_STATUS_FLAG = 2
-	// Generator is generating power.
-	GENERATOR_STATUS_FLAG_GENERATING GENERATOR_STATUS_FLAG = 4
-	// Generator is charging the batteries (generating enough power to charge and provide the load).
-	GENERATOR_STATUS_FLAG_CHARGING GENERATOR_STATUS_FLAG = 8
-	// Generator is operating at a reduced maximum power.
-	GENERATOR_STATUS_FLAG_REDUCED_POWER GENERATOR_STATUS_FLAG = 16
-	// Generator is providing the maximum output.
-	GENERATOR_STATUS_FLAG_MAXPOWER GENERATOR_STATUS_FLAG = 32
-	// Generator is near the maximum operating temperature, cooling is insufficient.
-	GENERATOR_STATUS_FLAG_OVERTEMP_WARNING GENERATOR_STATUS_FLAG = 64
-	// Generator hit the maximum operating temperature and shutdown.
-	GENERATOR_STATUS_FLAG_OVERTEMP_FAULT GENERATOR_STATUS_FLAG = 128
-	// Power electronics are near the maximum operating temperature, cooling is insufficient.
-	GENERATOR_STATUS_FLAG_ELECTRONICS_OVERTEMP_WARNING GENERATOR_STATUS_FLAG = 256
-	// Power electronics hit the maximum operating temperature and shutdown.
-	GENERATOR_STATUS_FLAG_ELECTRONICS_OVERTEMP_FAULT GENERATOR_STATUS_FLAG = 512
-	// Power electronics experienced a fault and shutdown.
-	GENERATOR_STATUS_FLAG_ELECTRONICS_FAULT GENERATOR_STATUS_FLAG = 1024
-	// The power source supplying the generator failed e.g. mechanical generator stopped, tether is no longer providing power, solar cell is in shade, hydrogen reaction no longer happening.
-	GENERATOR_STATUS_FLAG_POWERSOURCE_FAULT GENERATOR_STATUS_FLAG = 2048
-	// Generator controller having communication problems.
-	GENERATOR_STATUS_FLAG_COMMUNICATION_WARNING GENERATOR_STATUS_FLAG = 4096
-	// Power electronic or generator cooling system error.
-	GENERATOR_STATUS_FLAG_COOLING_WARNING GENERATOR_STATUS_FLAG = 8192
-	// Generator controller power rail experienced a fault.
-	GENERATOR_STATUS_FLAG_POWER_RAIL_FAULT GENERATOR_STATUS_FLAG = 16384
-	// Generator controller exceeded the overcurrent threshold and shutdown to prevent damage.
-	GENERATOR_STATUS_FLAG_OVERCURRENT_FAULT GENERATOR_STATUS_FLAG = 32768
-	// Generator controller detected a high current going into the batteries and shutdown to prevent battery damage.
-	GENERATOR_STATUS_FLAG_BATTERY_OVERCHARGE_CURRENT_FAULT GENERATOR_STATUS_FLAG = 65536
-	// Generator controller exceeded it's overvoltage threshold and shutdown to prevent it exceeding the voltage rating.
-	GENERATOR_STATUS_FLAG_OVERVOLTAGE_FAULT GENERATOR_STATUS_FLAG = 131072
-)
-
-// MarshalText implements the encoding.TextMarshaler interface.
-func (e GENERATOR_STATUS_FLAG) MarshalText() ([]byte, error) {
-	switch e {
-	case GENERATOR_STATUS_FLAG_OFF:
-		return []byte("GENERATOR_STATUS_FLAG_OFF"), nil
-	case GENERATOR_STATUS_FLAG_READY:
-		return []byte("GENERATOR_STATUS_FLAG_READY"), nil
-	case GENERATOR_STATUS_FLAG_GENERATING:
-		return []byte("GENERATOR_STATUS_FLAG_GENERATING"), nil
-	case GENERATOR_STATUS_FLAG_CHARGING:
-		return []byte("GENERATOR_STATUS_FLAG_CHARGING"), nil
-	case GENERATOR_STATUS_FLAG_REDUCED_POWER:
-		return []byte("GENERATOR_STATUS_FLAG_REDUCED_POWER"), nil
-	case GENERATOR_STATUS_FLAG_MAXPOWER:
-		return []byte("GENERATOR_STATUS_FLAG_MAXPOWER"), nil
-	case GENERATOR_STATUS_FLAG_OVERTEMP_WARNING:
-		return []byte("GENERATOR_STATUS_FLAG_OVERTEMP_WARNING"), nil
-	case GENERATOR_STATUS_FLAG_OVERTEMP_FAULT:
-		return []byte("GENERATOR_STATUS_FLAG_OVERTEMP_FAULT"), nil
-	case GENERATOR_STATUS_FLAG_ELECTRONICS_OVERTEMP_WARNING:
-		return []byte("GENERATOR_STATUS_FLAG_ELECTRONICS_OVERTEMP_WARNING"), nil
-	case GENERATOR_STATUS_FLAG_ELECTRONICS_OVERTEMP_FAULT:
-		return []byte("GENERATOR_STATUS_FLAG_ELECTRONICS_OVERTEMP_FAULT"), nil
-	case GENERATOR_STATUS_FLAG_ELECTRONICS_FAULT:
-		return []byte("GENERATOR_STATUS_FLAG_ELECTRONICS_FAULT"), nil
-	case GENERATOR_STATUS_FLAG_POWERSOURCE_FAULT:
-		return []byte("GENERATOR_STATUS_FLAG_POWERSOURCE_FAULT"), nil
-	case GENERATOR_STATUS_FLAG_COMMUNICATION_WARNING:
-		return []byte("GENERATOR_STATUS_FLAG_COMMUNICATION_WARNING"), nil
-	case GENERATOR_STATUS_FLAG_COOLING_WARNING:
-		return []byte("GENERATOR_STATUS_FLAG_COOLING_WARNING"), nil
-	case GENERATOR_STATUS_FLAG_POWER_RAIL_FAULT:
-		return []byte("GENERATOR_STATUS_FLAG_POWER_RAIL_FAULT"), nil
-	case GENERATOR_STATUS_FLAG_OVERCURRENT_FAULT:
-		return []byte("GENERATOR_STATUS_FLAG_OVERCURRENT_FAULT"), nil
-	case GENERATOR_STATUS_FLAG_BATTERY_OVERCHARGE_CURRENT_FAULT:
-		return []byte("GENERATOR_STATUS_FLAG_BATTERY_OVERCHARGE_CURRENT_FAULT"), nil
-	case GENERATOR_STATUS_FLAG_OVERVOLTAGE_FAULT:
-		return []byte("GENERATOR_STATUS_FLAG_OVERVOLTAGE_FAULT"), nil
-	}
-	return nil, errors.New("invalid value")
-}
-
-// UnmarshalText implements the encoding.TextUnmarshaler interface.
-func (e *GENERATOR_STATUS_FLAG) UnmarshalText(text []byte) error {
-	switch string(text) {
-	case "GENERATOR_STATUS_FLAG_OFF":
-		*e = GENERATOR_STATUS_FLAG_OFF
-		return nil
-	case "GENERATOR_STATUS_FLAG_READY":
-		*e = GENERATOR_STATUS_FLAG_READY
-		return nil
-	case "GENERATOR_STATUS_FLAG_GENERATING":
-		*e = GENERATOR_STATUS_FLAG_GENERATING
-		return nil
-	case "GENERATOR_STATUS_FLAG_CHARGING":
-		*e = GENERATOR_STATUS_FLAG_CHARGING
-		return nil
-	case "GENERATOR_STATUS_FLAG_REDUCED_POWER":
-		*e = GENERATOR_STATUS_FLAG_REDUCED_POWER
-		return nil
-	case "GENERATOR_STATUS_FLAG_MAXPOWER":
-		*e = GENERATOR_STATUS_FLAG_MAXPOWER
-		return nil
-	case "GENERATOR_STATUS_FLAG_OVERTEMP_WARNING":
-		*e = GENERATOR_STATUS_FLAG_OVERTEMP_WARNING
-		return nil
-	case "GENERATOR_STATUS_FLAG_OVERTEMP_FAULT":
-		*e = GENERATOR_STATUS_FLAG_OVERTEMP_FAULT
-		return nil
-	case "GENERATOR_STATUS_FLAG_ELECTRONICS_OVERTEMP_WARNING":
-		*e = GENERATOR_STATUS_FLAG_ELECTRONICS_OVERTEMP_WARNING
-		return nil
-	case "GENERATOR_STATUS_FLAG_ELECTRONICS_OVERTEMP_FAULT":
-		*e = GENERATOR_STATUS_FLAG_ELECTRONICS_OVERTEMP_FAULT
-		return nil
-	case "GENERATOR_STATUS_FLAG_ELECTRONICS_FAULT":
-		*e = GENERATOR_STATUS_FLAG_ELECTRONICS_FAULT
-		return nil
-	case "GENERATOR_STATUS_FLAG_POWERSOURCE_FAULT":
-		*e = GENERATOR_STATUS_FLAG_POWERSOURCE_FAULT
-		return nil
-	case "GENERATOR_STATUS_FLAG_COMMUNICATION_WARNING":
-		*e = GENERATOR_STATUS_FLAG_COMMUNICATION_WARNING
-		return nil
-	case "GENERATOR_STATUS_FLAG_COOLING_WARNING":
-		*e = GENERATOR_STATUS_FLAG_COOLING_WARNING
-		return nil
-	case "GENERATOR_STATUS_FLAG_POWER_RAIL_FAULT":
-		*e = GENERATOR_STATUS_FLAG_POWER_RAIL_FAULT
-		return nil
-	case "GENERATOR_STATUS_FLAG_OVERCURRENT_FAULT":
-		*e = GENERATOR_STATUS_FLAG_OVERCURRENT_FAULT
-		return nil
-	case "GENERATOR_STATUS_FLAG_BATTERY_OVERCHARGE_CURRENT_FAULT":
-		*e = GENERATOR_STATUS_FLAG_BATTERY_OVERCHARGE_CURRENT_FAULT
-		return nil
-	case "GENERATOR_STATUS_FLAG_OVERVOLTAGE_FAULT":
-		*e = GENERATOR_STATUS_FLAG_OVERVOLTAGE_FAULT
-		return nil
-	}
-	return errors.New("invalid value")
-}
-
-// String implements the fmt.Stringer interface.
-func (e GENERATOR_STATUS_FLAG) String() string {
-	byts, err := e.MarshalText()
-	if err == nil {
-		return string(byts)
-	}
-	return strconv.FormatInt(int64(e), 10)
-}
-
 // Gimbal device (low level) capability flags (bitmap)
 type GIMBAL_DEVICE_CAP_FLAGS int
 
@@ -6711,6 +6556,182 @@ func (e *MAV_FRAME) UnmarshalText(text []byte) error {
 
 // String implements the fmt.Stringer interface.
 func (e MAV_FRAME) String() string {
+	byts, err := e.MarshalText()
+	if err == nil {
+		return string(byts)
+	}
+	return strconv.FormatInt(int64(e), 10)
+}
+
+// Flags to report status/failure cases for a power generator (used in GENERATOR_STATUS). Note that FAULTS are conditions that cause the generator to fail. Warnings are conditions that require attention before the next use (they indicate the system is not operating properly).
+type MAV_GENERATOR_STATUS_FLAG int
+
+const (
+	// Generator is off.
+	MAV_GENERATOR_STATUS_FLAG_OFF MAV_GENERATOR_STATUS_FLAG = 1
+	// Generator is ready to start generating power.
+	MAV_GENERATOR_STATUS_FLAG_READY MAV_GENERATOR_STATUS_FLAG = 2
+	// Generator is generating power.
+	MAV_GENERATOR_STATUS_FLAG_GENERATING MAV_GENERATOR_STATUS_FLAG = 4
+	// Generator is charging the batteries (generating enough power to charge and provide the load).
+	MAV_GENERATOR_STATUS_FLAG_CHARGING MAV_GENERATOR_STATUS_FLAG = 8
+	// Generator is operating at a reduced maximum power.
+	MAV_GENERATOR_STATUS_FLAG_REDUCED_POWER MAV_GENERATOR_STATUS_FLAG = 16
+	// Generator is providing the maximum output.
+	MAV_GENERATOR_STATUS_FLAG_MAXPOWER MAV_GENERATOR_STATUS_FLAG = 32
+	// Generator is near the maximum operating temperature, cooling is insufficient.
+	MAV_GENERATOR_STATUS_FLAG_OVERTEMP_WARNING MAV_GENERATOR_STATUS_FLAG = 64
+	// Generator hit the maximum operating temperature and shutdown.
+	MAV_GENERATOR_STATUS_FLAG_OVERTEMP_FAULT MAV_GENERATOR_STATUS_FLAG = 128
+	// Power electronics are near the maximum operating temperature, cooling is insufficient.
+	MAV_GENERATOR_STATUS_FLAG_ELECTRONICS_OVERTEMP_WARNING MAV_GENERATOR_STATUS_FLAG = 256
+	// Power electronics hit the maximum operating temperature and shutdown.
+	MAV_GENERATOR_STATUS_FLAG_ELECTRONICS_OVERTEMP_FAULT MAV_GENERATOR_STATUS_FLAG = 512
+	// Power electronics experienced a fault and shutdown.
+	MAV_GENERATOR_STATUS_FLAG_ELECTRONICS_FAULT MAV_GENERATOR_STATUS_FLAG = 1024
+	// The power source supplying the generator failed e.g. mechanical generator stopped, tether is no longer providing power, solar cell is in shade, hydrogen reaction no longer happening.
+	MAV_GENERATOR_STATUS_FLAG_POWERSOURCE_FAULT MAV_GENERATOR_STATUS_FLAG = 2048
+	// Generator controller having communication problems.
+	MAV_GENERATOR_STATUS_FLAG_COMMUNICATION_WARNING MAV_GENERATOR_STATUS_FLAG = 4096
+	// Power electronic or generator cooling system error.
+	MAV_GENERATOR_STATUS_FLAG_COOLING_WARNING MAV_GENERATOR_STATUS_FLAG = 8192
+	// Generator controller power rail experienced a fault.
+	MAV_GENERATOR_STATUS_FLAG_POWER_RAIL_FAULT MAV_GENERATOR_STATUS_FLAG = 16384
+	// Generator controller exceeded the overcurrent threshold and shutdown to prevent damage.
+	MAV_GENERATOR_STATUS_FLAG_OVERCURRENT_FAULT MAV_GENERATOR_STATUS_FLAG = 32768
+	// Generator controller detected a high current going into the batteries and shutdown to prevent battery damage.
+	MAV_GENERATOR_STATUS_FLAG_BATTERY_OVERCHARGE_CURRENT_FAULT MAV_GENERATOR_STATUS_FLAG = 65536
+	// Generator controller exceeded it's overvoltage threshold and shutdown to prevent it exceeding the voltage rating.
+	MAV_GENERATOR_STATUS_FLAG_OVERVOLTAGE_FAULT MAV_GENERATOR_STATUS_FLAG = 131072
+	// Batteries are under voltage (generator will not start).
+	MAV_GENERATOR_STATUS_FLAG_BATTERY_UNDERVOLT_FAULT MAV_GENERATOR_STATUS_FLAG = 262144
+	// Generator start is inhibited by e.g. a safety switch.
+	MAV_GENERATOR_STATUS_FLAG_START_INHIBITED MAV_GENERATOR_STATUS_FLAG = 524288
+	// Generator requires maintenance.
+	MAV_GENERATOR_STATUS_FLAG_MAINTENANCE_REQUIRED MAV_GENERATOR_STATUS_FLAG = 1048576
+)
+
+// MarshalText implements the encoding.TextMarshaler interface.
+func (e MAV_GENERATOR_STATUS_FLAG) MarshalText() ([]byte, error) {
+	switch e {
+	case MAV_GENERATOR_STATUS_FLAG_OFF:
+		return []byte("MAV_GENERATOR_STATUS_FLAG_OFF"), nil
+	case MAV_GENERATOR_STATUS_FLAG_READY:
+		return []byte("MAV_GENERATOR_STATUS_FLAG_READY"), nil
+	case MAV_GENERATOR_STATUS_FLAG_GENERATING:
+		return []byte("MAV_GENERATOR_STATUS_FLAG_GENERATING"), nil
+	case MAV_GENERATOR_STATUS_FLAG_CHARGING:
+		return []byte("MAV_GENERATOR_STATUS_FLAG_CHARGING"), nil
+	case MAV_GENERATOR_STATUS_FLAG_REDUCED_POWER:
+		return []byte("MAV_GENERATOR_STATUS_FLAG_REDUCED_POWER"), nil
+	case MAV_GENERATOR_STATUS_FLAG_MAXPOWER:
+		return []byte("MAV_GENERATOR_STATUS_FLAG_MAXPOWER"), nil
+	case MAV_GENERATOR_STATUS_FLAG_OVERTEMP_WARNING:
+		return []byte("MAV_GENERATOR_STATUS_FLAG_OVERTEMP_WARNING"), nil
+	case MAV_GENERATOR_STATUS_FLAG_OVERTEMP_FAULT:
+		return []byte("MAV_GENERATOR_STATUS_FLAG_OVERTEMP_FAULT"), nil
+	case MAV_GENERATOR_STATUS_FLAG_ELECTRONICS_OVERTEMP_WARNING:
+		return []byte("MAV_GENERATOR_STATUS_FLAG_ELECTRONICS_OVERTEMP_WARNING"), nil
+	case MAV_GENERATOR_STATUS_FLAG_ELECTRONICS_OVERTEMP_FAULT:
+		return []byte("MAV_GENERATOR_STATUS_FLAG_ELECTRONICS_OVERTEMP_FAULT"), nil
+	case MAV_GENERATOR_STATUS_FLAG_ELECTRONICS_FAULT:
+		return []byte("MAV_GENERATOR_STATUS_FLAG_ELECTRONICS_FAULT"), nil
+	case MAV_GENERATOR_STATUS_FLAG_POWERSOURCE_FAULT:
+		return []byte("MAV_GENERATOR_STATUS_FLAG_POWERSOURCE_FAULT"), nil
+	case MAV_GENERATOR_STATUS_FLAG_COMMUNICATION_WARNING:
+		return []byte("MAV_GENERATOR_STATUS_FLAG_COMMUNICATION_WARNING"), nil
+	case MAV_GENERATOR_STATUS_FLAG_COOLING_WARNING:
+		return []byte("MAV_GENERATOR_STATUS_FLAG_COOLING_WARNING"), nil
+	case MAV_GENERATOR_STATUS_FLAG_POWER_RAIL_FAULT:
+		return []byte("MAV_GENERATOR_STATUS_FLAG_POWER_RAIL_FAULT"), nil
+	case MAV_GENERATOR_STATUS_FLAG_OVERCURRENT_FAULT:
+		return []byte("MAV_GENERATOR_STATUS_FLAG_OVERCURRENT_FAULT"), nil
+	case MAV_GENERATOR_STATUS_FLAG_BATTERY_OVERCHARGE_CURRENT_FAULT:
+		return []byte("MAV_GENERATOR_STATUS_FLAG_BATTERY_OVERCHARGE_CURRENT_FAULT"), nil
+	case MAV_GENERATOR_STATUS_FLAG_OVERVOLTAGE_FAULT:
+		return []byte("MAV_GENERATOR_STATUS_FLAG_OVERVOLTAGE_FAULT"), nil
+	case MAV_GENERATOR_STATUS_FLAG_BATTERY_UNDERVOLT_FAULT:
+		return []byte("MAV_GENERATOR_STATUS_FLAG_BATTERY_UNDERVOLT_FAULT"), nil
+	case MAV_GENERATOR_STATUS_FLAG_START_INHIBITED:
+		return []byte("MAV_GENERATOR_STATUS_FLAG_START_INHIBITED"), nil
+	case MAV_GENERATOR_STATUS_FLAG_MAINTENANCE_REQUIRED:
+		return []byte("MAV_GENERATOR_STATUS_FLAG_MAINTENANCE_REQUIRED"), nil
+	}
+	return nil, errors.New("invalid value")
+}
+
+// UnmarshalText implements the encoding.TextUnmarshaler interface.
+func (e *MAV_GENERATOR_STATUS_FLAG) UnmarshalText(text []byte) error {
+	switch string(text) {
+	case "MAV_GENERATOR_STATUS_FLAG_OFF":
+		*e = MAV_GENERATOR_STATUS_FLAG_OFF
+		return nil
+	case "MAV_GENERATOR_STATUS_FLAG_READY":
+		*e = MAV_GENERATOR_STATUS_FLAG_READY
+		return nil
+	case "MAV_GENERATOR_STATUS_FLAG_GENERATING":
+		*e = MAV_GENERATOR_STATUS_FLAG_GENERATING
+		return nil
+	case "MAV_GENERATOR_STATUS_FLAG_CHARGING":
+		*e = MAV_GENERATOR_STATUS_FLAG_CHARGING
+		return nil
+	case "MAV_GENERATOR_STATUS_FLAG_REDUCED_POWER":
+		*e = MAV_GENERATOR_STATUS_FLAG_REDUCED_POWER
+		return nil
+	case "MAV_GENERATOR_STATUS_FLAG_MAXPOWER":
+		*e = MAV_GENERATOR_STATUS_FLAG_MAXPOWER
+		return nil
+	case "MAV_GENERATOR_STATUS_FLAG_OVERTEMP_WARNING":
+		*e = MAV_GENERATOR_STATUS_FLAG_OVERTEMP_WARNING
+		return nil
+	case "MAV_GENERATOR_STATUS_FLAG_OVERTEMP_FAULT":
+		*e = MAV_GENERATOR_STATUS_FLAG_OVERTEMP_FAULT
+		return nil
+	case "MAV_GENERATOR_STATUS_FLAG_ELECTRONICS_OVERTEMP_WARNING":
+		*e = MAV_GENERATOR_STATUS_FLAG_ELECTRONICS_OVERTEMP_WARNING
+		return nil
+	case "MAV_GENERATOR_STATUS_FLAG_ELECTRONICS_OVERTEMP_FAULT":
+		*e = MAV_GENERATOR_STATUS_FLAG_ELECTRONICS_OVERTEMP_FAULT
+		return nil
+	case "MAV_GENERATOR_STATUS_FLAG_ELECTRONICS_FAULT":
+		*e = MAV_GENERATOR_STATUS_FLAG_ELECTRONICS_FAULT
+		return nil
+	case "MAV_GENERATOR_STATUS_FLAG_POWERSOURCE_FAULT":
+		*e = MAV_GENERATOR_STATUS_FLAG_POWERSOURCE_FAULT
+		return nil
+	case "MAV_GENERATOR_STATUS_FLAG_COMMUNICATION_WARNING":
+		*e = MAV_GENERATOR_STATUS_FLAG_COMMUNICATION_WARNING
+		return nil
+	case "MAV_GENERATOR_STATUS_FLAG_COOLING_WARNING":
+		*e = MAV_GENERATOR_STATUS_FLAG_COOLING_WARNING
+		return nil
+	case "MAV_GENERATOR_STATUS_FLAG_POWER_RAIL_FAULT":
+		*e = MAV_GENERATOR_STATUS_FLAG_POWER_RAIL_FAULT
+		return nil
+	case "MAV_GENERATOR_STATUS_FLAG_OVERCURRENT_FAULT":
+		*e = MAV_GENERATOR_STATUS_FLAG_OVERCURRENT_FAULT
+		return nil
+	case "MAV_GENERATOR_STATUS_FLAG_BATTERY_OVERCHARGE_CURRENT_FAULT":
+		*e = MAV_GENERATOR_STATUS_FLAG_BATTERY_OVERCHARGE_CURRENT_FAULT
+		return nil
+	case "MAV_GENERATOR_STATUS_FLAG_OVERVOLTAGE_FAULT":
+		*e = MAV_GENERATOR_STATUS_FLAG_OVERVOLTAGE_FAULT
+		return nil
+	case "MAV_GENERATOR_STATUS_FLAG_BATTERY_UNDERVOLT_FAULT":
+		*e = MAV_GENERATOR_STATUS_FLAG_BATTERY_UNDERVOLT_FAULT
+		return nil
+	case "MAV_GENERATOR_STATUS_FLAG_START_INHIBITED":
+		*e = MAV_GENERATOR_STATUS_FLAG_START_INHIBITED
+		return nil
+	case "MAV_GENERATOR_STATUS_FLAG_MAINTENANCE_REQUIRED":
+		*e = MAV_GENERATOR_STATUS_FLAG_MAINTENANCE_REQUIRED
+		return nil
+	}
+	return errors.New("invalid value")
+}
+
+// String implements the fmt.Stringer interface.
+func (e MAV_GENERATOR_STATUS_FLAG) String() string {
 	byts, err := e.MarshalText()
 	if err == nil {
 		return string(byts)
@@ -16140,7 +16161,7 @@ func (*MessageSmartBatteryStatus) GetId() uint32 {
 // Telemetry of power generation system. Alternator or mechanical generator.
 type MessageGeneratorStatus struct {
 	// Status flags.
-	Status GENERATOR_STATUS_FLAG `mavenum:"uint64"`
+	Status MAV_GENERATOR_STATUS_FLAG `mavenum:"uint64"`
 	// Speed of electrical generator or alternator. UINT16_MAX: field not provided.
 	GeneratorSpeed uint16
 	// Current into/out of battery. Positive for out. Negative for in. NaN: field not provided.

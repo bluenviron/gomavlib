259593f0cdb2f3404197293528b6ed0e24fc8075
aler9 <46489434+aler9@users.noreply.github.com>
2021-12-01 11:39:49 +0100

    improve coverage

---
+++ pkg/dialect/decencoder_test.go
@@ -0,0 +1,34 @@
+package dialect
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/aler9/gomavlib/pkg/msg"
+)
+
+type (
+	MAV_TYPE      int //nolint:golint
+	MAV_AUTOPILOT int //nolint:golint
+	MAV_MODE_FLAG int //nolint:golint
+	MAV_STATE     int //nolint:golint
+)
+
+type MessageHeartbeat struct {
+	Type           MAV_TYPE      `mavenum:"uint8"`
+	Autopilot      MAV_AUTOPILOT `mavenum:"uint8"`
+	BaseMode       MAV_MODE_FLAG `mavenum:"uint8"`
+	CustomMode     uint32
+	SystemStatus   MAV_STATE `mavenum:"uint8"`
+	MavlinkVersion uint8
+}
+
+func (*MessageHeartbeat) GetID() uint32 {
+	return 0
+}
+
+func TestDecEncoder(t *testing.T) {
+	_, err := NewDecEncoder(&Dialect{3, []msg.Message{&MessageHeartbeat{}}})
+	require.NoError(t, err)
+}

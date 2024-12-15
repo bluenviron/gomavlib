8391bff4d4a0a2f4a2f052a0007054260a50d51c
Chris Dalke <chrisdalke@gmail.com>
2024-12-15 17:26:22 -0500

    feat: Switch serial library to one with larger cross-platform support (#138)
    * Change the serial lib
    
    * Renamed package
    
    * use forked serial lib
    
    * request all data streams
    
    * Move back to upstream serial library
    
    * Revert name change
    
    * Change stream request
    
    * Add hardware flow control enable
    
    * Go fmt
    
    * run gofumpt
    
    ---------
    
    Co-authored-by: aler9 <46489434+aler9@users.noreply.github.com>

--- endpoint_serial.go
+++ endpoint_serial.go
@@ -4,16 +4,26 @@ import (
 	"context"
 	"io"
 
-	"github.com/tarm/serial"
+	"go.bug.st/serial"
 
 	"github.com/bluenviron/gomavlib/v3/pkg/reconnector"
 )
 
 var serialOpenFunc = func(device string, baud int) (io.ReadWriteCloser, error) {
-	return serial.OpenPort(&serial.Config{
-		Name: device,
-		Baud: baud,
+	dev, err := serial.Open(device, &serial.Mode{
+		BaudRate: baud,
+		Parity:   serial.NoParity,
+		DataBits: 8,
+		StopBits: serial.OneStopBit,
 	})
+	if err != nil {
+		return nil, err
+	}
+
+	dev.SetDTR(true) //nolint:errcheck
+	dev.SetRTS(true) //nolint:errcheck
+
+	return dev, nil
 }
 
 // EndpointSerial sets up a endpoint that works with a serial port.
